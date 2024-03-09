ee8ea0887bdf5b40d1a0fe3cada7c492f2c60221
kiranbchitari <kiranbchitari@gmail.com>
2024-03-09 22:00:25 +0530

    Inital Commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Github() {
+    return (<>
+        <div className="bg-gray-600 text-3xl text-centre">
+            Github
+        </div>
+    </>)
+}
+export default Github;
